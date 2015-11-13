b105530b62741fa7b2823ecc906706061fe3e067
Kenny Zhang <Kenny.y.zhang@gmail.com>
2015-11-13 11:58:21 -0800

    add assessment

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-app = angular.module('todoApp'); // What's missing? Hint, it's more than one thing.
+var app = angular.module('todoApp', []); // What's missing? Hint, it's more than one thing.
 
-app.controller("TodosController", function() { // What's missing?
-	$scope.todos = ; // Store todo items here. What is the data type?
-	function addTodo() { // Rewrite to make this function abailable in your view
-		// Add code here
+app.controller("todosController", function($scope) { // What's missing?
+	$scope.todos = {}; // Store todo items here. What is the data type?
+	$scope.addTodo = function(task) { 
+    $scope.todos.push({todo:task.text});
+    $scope.todos.task = "";
 	};
-	function deleteAll() { // Rewrite to make this function abailable in your view
-		// Add code here
+	$scope.deleteAll = function() { // Rewrite to make this function available in your view
+		$scope.todos = {};
 	};
 });
